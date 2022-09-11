6567040efdaff24eddd24d2655ab2f28768ba4ef
darrelaustero2022 <darrelwork2022@yahoo.com>
2022-09-11 18:12:48 +0800

    fixing folder filenames

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,10 @@ import Nav from './Components/nav/Nav'
 import About from './Components/About/About'
 import Experience from './Components/Experience/Experience'
 import Services from './Components/Services/Services'
-import Portfolio from './Components/Portfolio/Portfolio'
+import Portfolio from './Components/portfolio/Portfolio'
 import Testimonials from './Components/Testimonials/Testimonials'
 import Contact from './Components/Contact/Contact'
-import Footer from './Components/Footer/Footer'
+import Footer from './Components/footer/Footer'
 
 
 
