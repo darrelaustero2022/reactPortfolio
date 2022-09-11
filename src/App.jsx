060d7414d85362ac357a9302160e9a7947a4be6a
darrelaustero2022 <darrelwork2022@yahoo.com>
2022-09-11 18:06:56 +0800

    github error

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Header from './Components/Header/Header'
-import Nav from './Components/Nav/Nav'
+import Nav from './Components/nav/Nav'
 import About from './Components/About/About'
 import Experience from './Components/Experience/Experience'
 import Services from './Components/Services/Services'
