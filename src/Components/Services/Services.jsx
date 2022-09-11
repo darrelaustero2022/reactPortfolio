7b2206f660bd5ff21da51b40102f529113909d3b
darrelaustero2022 <darrelwork2022@yahoo.com>
2022-09-11 20:27:57 +0800

    srvcs

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -11,7 +11,60 @@ const Services = () => {
       <div className="container services__container">
         <article className="service">
           <div className="service__head">
+            <h3>Ux/Ui Design</h3>
 
+          </div>
+          /*============================= Ux/Ui Design==================*/
+          <ul className="service__list">
+            <li>
+              <GiCheckMark className='service__list-icon'/>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut, laboriosam.</p>
+            </li>
+            <li>
+              <GiCheckMark className='service__list-icon'/>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut, laboriosam.</p>
+            </li>
+            <li>
+              <GiCheckMark className='service__list-icon'/>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut, laboriosam.</p>
+            </li>
+            <li>
+              <GiCheckMark className='service__list-icon'/>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut, laboriosam.</p>
+            </li>
+          </ul>
+        </article>
+        /*=============web development=================*/
+        <article className="service">
+          <div className="service__head">
+            <h3>Web Development</h3>
+            
+          </div>
+
+          <ul className="service__list">
+            <li>
+              <GiCheckMark className='service__list-icon'/>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut, laboriosam.</p>
+            </li>
+            <li>
+              <GiCheckMark className='service__list-icon'/>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut, laboriosam.</p>
+            </li>
+            <li>
+              <GiCheckMark className='service__list-icon'/>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut, laboriosam.</p>
+            </li>
+            <li>
+              <GiCheckMark className='service__list-icon'/>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut, laboriosam.</p>
+            </li>
+          </ul>
+        </article>
+        /*==================== Social Media =====================*/
+        <article className="service">
+          <div className="service__head">
+            <h3>Social Media</h3>
+            
           </div>
 
           <ul className="service__list">
