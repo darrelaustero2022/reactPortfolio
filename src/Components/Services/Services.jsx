57c47a256f615dde652cad74cb0c8dd2543e09e2
darrelaustero2022 <darrelwork2022@yahoo.com>
2022-09-11 20:04:34 +0800

    added services section

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -1,11 +1,39 @@
 import React from 'react'
 import './services.css'
+import {GiCheckMark} from 'react-icons/gi'
 
 const Services = () => {
   return (
     <section id='services'>
-      <h5>What I can Offer</h5>
+      <h5>What I Offer</h5>
       <h2>Services</h2>
+
+      <div className="container services__container">
+        <article className="service">
+          <div className="service__head">
+
+          </div>
+
+          <ul className="service__list">
+            <li>
+              <GiCheckMark className='service__list-icon'/>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut, laboriosam.</p>
+            </li>
+            <li>
+              <GiCheckMark className='service__list-icon'/>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut, laboriosam.</p>
+            </li>
+            <li>
+              <GiCheckMark className='service__list-icon'/>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut, laboriosam.</p>
+            </li>
+            <li>
+              <GiCheckMark className='service__list-icon'/>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut, laboriosam.</p>
+            </li>
+          </ul>
+        </article>
+      </div>
     </section>
   )
 }
